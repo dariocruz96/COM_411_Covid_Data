63be4f2c8362bc898e1a33f0899578f600c5c80d
dariocruz96 <dariocruz96@hotmail.com>
2022-01-13 20:13:01 +0000

    Added code for task 20 - Retrieve a summary of all of the records

--- process.py
+++ process.py
@@ -1,31 +1,3 @@
-"""
-This module is responsible for processing the data.  Each function in this module will take a list of records,
-process it and return the desired result.
-"""
-
-"""
-Task 16 - 20: Write suitable functions to process the data.
-
-Each of the functions below should follow the pattern:
-- Take a list of records (where each record is a list of data values) as a parameter.
-- Process the list of records appropriately.  You may use the module 'tui' to retrieve any additional information 
-required from the user to complete the processing.
-- Return a suitable result
-
-The required functions are as follows:
-- Retrieve the total number of records that have been loaded.
-- Retrieve a record with the serial number as specified by the user.
-- Retrieve the records for the observation dates as specified by the user.
-- Retrieve all of the records grouped by the country/region.
-- Retrieve a summary of all of the records. This should include the following information for each country/region:
-    - the total number of confirmed cases
-    - the total number of deaths
-    - the total number of recoveries
-
- 
-"""
-
-# TODO: Your code here
 import tui
 
 
@@ -58,3 +30,25 @@ def group_by_country_region(covid_records):
     group.sort(key=lambda x: x[3])
     return group
 
+def summary_of_records(covid_records):
+    summary = {}
+    summary_list = []
+    for record in covid_records:
+        country_region = record[3]
+        cases = record[-3]
+        deaths = record[-2]
+        recoveries = record[-1]
+        if country_region in summary:
+            summary[country_region]['Cases'] += int(cases)
+            summary[country_region]['Deaths'] += int(deaths)
+            summary[country_region]['Recoveries'] += int(recoveries)
+        else:
+            summary[country_region] = {'Cases': 0, 'Deaths': 0, 'Recoveries': 0}
+            summary[country_region]['Cases'] += int(cases)
+            summary[country_region]['Deaths'] += int(deaths)
+            summary[country_region]['Recoveries'] += int(recoveries)
+
+    for key, value in summary.items():
+        summary_list.append((key, value))
+
+    return summary_list
